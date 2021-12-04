fcfba7ab89b155448187ffb96b7ee9cf7d3f9a22
anshul <anshul.bansod2@gmail.com>
2021-12-04 16:36:14 +0530

    Final

---
+++ irisapi/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class PredResults(models.Model):
+
+    sepal_length = models.FloatField()
+    sepal_width = models.FloatField()
+    petal_length = models.FloatField()
+    petal_width = models.FloatField()
